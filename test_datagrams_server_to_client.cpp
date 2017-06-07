08099b37ef486752b915219e7be21f1888eb5e78
Michał Kuźba <kmichael00968@gmail.com>
2017-06-08 01:23:45 +0200

    large datagrams testing

--- test_datagrams_server_to_client.cpp
+++ test_datagrams_server_to_client.cpp
@@ -62,5 +62,51 @@ int main(int argc, char* argv[]) {
 
     }
 
+    for (int i = 0; i < 100; i++)
+        events.push_back(new NewGame(11, 1200, 800, players_names_list));
+
+    DatagramServerToClient* dgram = new DatagramServerToClient(game_id, events);
+    vector<Datagram*> dgrams = dgram->datagrams();
+
+    cout << dgrams.size() << endl;
+
+    /* =============================================== */
+    cout << endl << endl;
+
+    DatagramServerToClient* parsed_dgram =
+            DatagramServerToClient::parse_datagram(dgrams[1]->get_data(), dgrams[1]->get_len());
+
+
+    cout << parsed_dgram->get_game_id() << endl;
+
+    cout << parsed_dgram->get_events().size() << endl;
+
+    for (Event* event: parsed_dgram->get_events()) {
+        uint8_t type = event->get_event_type();
+        cout << (unsigned long)type << " TYP "<< endl;
+
+        if (type == 0) {
+            NewGame* newGame = (NewGame*) event;
+            cout << newGame->get_maxx() << " " << newGame->get_maxy() << endl;
+            for (char* name: newGame->get_players_name_list()) {
+                cout << name << endl;
+            }
+        }
+        else if (type == 1) {
+            Pixel* pixel = (Pixel*) event;
+            cout << (unsigned long)(pixel->get_player_number()) << " " << pixel->get_x() << " " << pixel->get_y() << endl;
+        }
+        else if (type == 2) {
+            PlayerEliminated* playerEliminated = (PlayerEliminated*) event;
+            cout << (unsigned long) (playerEliminated->get_player_number()) << " PL eliminated pl number" << endl;
+        }
+        else if (type == 3) {
+            GameOver* gameOver = (GameOver*) event;
+            cout << (unsigned long)(gameOver->get_event_type()) << " GAME OVER" << endl;
+        }
+
+    }
+
+
     return 0;
 }
