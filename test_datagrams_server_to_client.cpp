a4745d16fc691a2dc9998ee98458e74e104abdd3
Michał Kuźba <kmichael00968@gmail.com>
2017-06-08 01:16:13 +0200

    test datagrams parsing

---
+++ test_datagrams_server_to_client.cpp
@@ -0,0 +1,66 @@
+#include <cstring>
+#include "server.h"
+#include "datagramServerToClient.h"
+#include <cassert>
+#include "event.h"
+#include "utils.h"
+
+int main(int argc, char* argv[]) {
+   Server server = Server(argc, argv);
+    uint32_t game_id = 30020;
+    vector<Event*> events;
+    Event* first = new GameOver(10);
+    events.push_back(first);
+    Event* second = new Pixel(12, 3, 4 ,5);
+    events.push_back(second);
+    Event* third = new PlayerEliminated(4, 44);
+    events.push_back(third);
+    vector<char*> players_names_list = {"Jan", "Marian", "Arnold" };
+    Event* fourth = new NewGame(11, 1200, 800, players_names_list);
+    events.push_back(fourth);
+
+    DatagramServerToClient* datagram = new DatagramServerToClient(game_id, events);
+    vector<Datagram*> datagrams = datagram->datagrams();
+
+    assert(datagrams.size() == 1);
+
+    uint32_t g_id;
+    memcpy(&g_id, datagrams[0]->get_data(), 4);
+    cout << ntohl(g_id) << endl;
+
+    DatagramServerToClient* parsed_datagram =
+            DatagramServerToClient::parse_datagram(datagrams[0]->get_data(), datagrams[0]->get_len());
+
+
+     cout << parsed_datagram->get_game_id() << endl;
+
+    cout << parsed_datagram->get_events().size() << endl;
+
+    for (Event* event: parsed_datagram->get_events()) {
+        uint8_t type = event->get_event_type();
+        cout << (unsigned long)type << " TYP "<< endl;
+
+        if (type == 0) {
+            NewGame* newGame = (NewGame*) event;
+            cout << newGame->get_maxx() << " " << newGame->get_maxy() << endl;
+            for (char* name: newGame->get_players_name_list()) {
+                cout << name << endl;
+            }
+        }
+        else if (type == 1) {
+            Pixel* pixel = (Pixel*) event;
+            cout << (unsigned long)(pixel->get_player_number()) << " " << pixel->get_x() << " " << pixel->get_y() << endl;
+        }
+        else if (type == 2) {
+            PlayerEliminated* playerEliminated = (PlayerEliminated*) event;
+            cout << (unsigned long) (playerEliminated->get_player_number()) << " PL eliminated pl number" << endl;
+        }
+        else if (type == 3) {
+            GameOver* gameOver = (GameOver*) event;
+            cout << (unsigned long)(gameOver->get_event_type()) << " GAME OVER" << endl;
+        }
+
+    }
+
+    return 0;
+}
